376e5b45f64598eef733909c59948d39128bce66
ahmed farid <engfaridooo@gmail.com>
2024-09-10 21:34:18 +0300

    Initial Commit

---
+++ IOSTask/IOSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTaskApp.swift
+//  IOSTask
+//
+//  Created by Farido on 10/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct IOSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
